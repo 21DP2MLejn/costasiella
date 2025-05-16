08b6d1d92420141668d42eebc434b8a14cd09646
Swukz <100911463+21DP2MLejn@users.noreply.github.com>
2025-05-16 15:04:09 +0300

    Update development.py

--- app/app/settings/development.py
+++ app/app/settings/development.py
@@ -111,8 +111,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'pulsefitnessauto@gmail.com'  # Your Gmail address
-EMAIL_HOST_PASSWORD = 'fiag wpjh knbe zsql'  # Gmail App Password
+EMAIL_HOST_USER = ''  # Your Gmail address
+EMAIL_HOST_PASSWORD = ''  # Gmail App Password
 
 
 # Detailed logging for debugging
